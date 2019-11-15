405b4c2e5973b2f77923f119525108ef67c4787a
jul <julislazy@gmail.com>
2019-11-15 22:36:49 +0100

    grid manager

--- source/ui.py
+++ source/ui.py
@@ -45,7 +45,6 @@ def on_closing():
 
 def backgroundthread():
     global midithread
-    print("penis")
     midithread = threading.Thread(target=run)
     midithread.start()
 
@@ -53,37 +52,46 @@ def _uisetup(inports: list, outports: list):
     global root
 
     root = tk.Tk()
-    root.title("EPIC AUTOMIDIHARMONIZER")
-    root.geometry('350x150')
+    root.title("AMH")
+    root.configure(padx=10, pady=10)
     root.resizable(False, False)
-    # devices
-    deviceframe = tk.Frame(root, pady=10)
-    deviceframe.pack(side=tk.TOP)
-    outportvar, inportvar = tk.StringVar(deviceframe), tk.StringVar(deviceframe)
-    outportvar.set(outports[0])
+    # input
+    inportvar = tk.StringVar(root)
     inportvar.set(inports[0])
-    setinports, setoutports = set(inports), set(outports)
-    outportmenu = tk.OptionMenu(deviceframe, outportvar, *setoutports)
-    outportmenu.pack(side=tk.LEFT)
-    inportmenu = tk.OptionMenu(deviceframe, inportvar, *setinports)
-    inportmenu.pack(side=tk.LEFT)
-    # scales
-    scaleframe = tk.Frame(root)
-    scaleframe.pack(side=tk.TOP)
-    scales = {"major", "minor"}
+    setinports = set(inports) 
+    inportlabel = tk.Label(root, text="IN:")
+    inportlabel.grid(row=0, column=0)
+    inportmenu = tk.OptionMenu(root, inportvar, *setinports)
+    inportmenu.grid(row=0, column=1, sticky=tk.W+tk.E)
+    # output
+    outportvar = tk.StringVar(root)
+    setoutports = set(outports)
+    outportvar.set(outports[0])
+    outportlabel = tk.Label(root, text="OUT:")
+    outportlabel.grid(row=1, column=0)
+    outportmenu = tk.OptionMenu(root, outportvar, *setoutports)
+    outportmenu.configure(width=len(max(outports, key=len)))
+    outportmenu.grid(row=1, column=1, sticky=tk.W+tk.E)
+    # keys
     keys = sorted({'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B'})
-    scalevar, keyvar = tk.StringVar(scaleframe), tk.StringVar(scaleframe)
-    scalevar.set("major")
+    keyvar = tk.StringVar(root)
     keyvar.set("C")
-    scalemenu = tk.OptionMenu(scaleframe, scalevar, *scales)
-    scalemenu.pack(side=tk.LEFT)
-    keymenu = tk.OptionMenu(scaleframe, keyvar, *keys)
-    keymenu.pack(side=tk.LEFT)
+    keylabel = tk.Label(root, text="KEY:")
+    keylabel.grid(row=2, column=0)
+    keymenu = tk.OptionMenu(root, keyvar, *keys)
+    keymenu.grid(row=2, column=1, sticky=tk.W+tk.E)
+    # scales
+    scales = {"major", "minor"}
+    scalevar = tk.StringVar(root)
+    scalevar.set("major")
+    scalelabel = tk.Label(root, text="SCALE:")
+    scalelabel.grid(row=3, column=0)
+    scalemenu = tk.OptionMenu(root, scalevar, *scales)
+    scalemenu.config()
+    scalemenu.grid(row=3, column=1, sticky=tk.W+tk.E)
     # apply button
-    applyframe = tk.Frame(root, pady=10)
-    applyframe.pack(side=tk.BOTTOM)
-    applybutton = tk.Button(applyframe, text=" Apply ", command=lambda: apply(inportvar.get(), outportvar.get(), scalevar.get(), keyvar.get(), inports, outports))
-    applybutton.pack()
+    applybutton = tk.Button(root, text=" Apply ", width=30, command=lambda: apply(inportvar.get(), outportvar.get(), scalevar.get(), keyvar.get(), inports, outports))
+    applybutton.grid(row=4, column=0, columnspan=2, sticky=tk.W+tk.E)
     root.protocol("WM_DELETE_WINDOW", on_closing)
     root.after(200, backgroundthread)
 
